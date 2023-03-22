a4525952d543f34dcc995d77e1174f1fbd325457
davidluengos <davidluengosanchez@hotmail.com>
2023-03-22 09:17:22 +0100

    corregido error cuando no hay partidos futuros y creada galería de staff técnico del equipo

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -20,11 +20,12 @@ class HomeController extends Controller
     {
         $proximoPartido = Partido::where('fecha', '>' , now())->orderBy('fecha')->first();
         $ultimosPartidos = Partido::where('fecha', '<' , now())->orderBy('fecha', 'desc')->take(3)->get();
-        $jugadorasPrimerEquipo = Jugadore::where('equipo_id', 3)->orderBy('numero')->get();
+        $jugadorasPrimerEquipo = Jugadore::where('equipo_id', 3)->where('rol_id', 1)->orderBy('numero')->get();
+        $staffPrimerEquipo = Jugadore::where('equipo_id', 3)->where('rol_id', "!=", 1)->orderBy('id')->get();
         //$patrocinadoresPrincipales = Patrocinadore::where('tipo_patrocinador_id',  3)->get();
         //$patrocinadoresInstitucionales = Patrocinadore::where('tipo_patrocinador_id', 4)->get();
         //$patrocinadoresNormales = Patrocinadore::where('tipo_patrocinador_id', 5)->get();
         $title = 'Inicio';
-        return view('front.home', compact('title', 'proximoPartido', 'ultimosPartidos', 'jugadorasPrimerEquipo'));
+        return view('front.home', compact('title', 'proximoPartido', 'ultimosPartidos', 'jugadorasPrimerEquipo', 'staffPrimerEquipo'));
     }
 }
--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -120,38 +120,44 @@
 
             <div class="col-lg-6">
                 <h1 class="display-5 mb-4">Próximo partido</h1>
-                <div class="row g-4 align-items-center">
-                    <div class="col-md-6">
-                        <div class="row g-4">
-                            <div class="col-12 wow fadeIn" data-wow-delay="0.3s">
-                                <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
-                                    <div class="">
-                                        <img src="{{$proximoPartido->equipoLocal->imagen}}" alt="{{$proximoPartido->equipoLocal->nombre}}" width="100">
+                @if ($proximoPartido == null)
+                    <div class="d-flex row btn btn-light py-3 px-4 justify-content-center align-items-center  fs-5 fw-bold text-primary">
+                        <p>No hay partidos a la vista</p>
+                    </div>
+                @else
+                    <div class="row g-4 align-items-center">
+                        <div class="col-md-6">
+                            <div class="row g-4">
+                                <div class="col-12 wow fadeIn" data-wow-delay="0.3s">
+                                    <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
+                                        <div class="">
+                                            <img src="{{$proximoPartido->equipoLocal->imagen}}" alt="{{$proximoPartido->equipoLocal->nombre}}" width="100">
+                                        </div>
+                                        <h4 class="mb-0">{{$proximoPartido->equipoLocal->nombre}}</h4>
                                     </div>
-                                    <h4 class="mb-0">{{$proximoPartido->equipoLocal->nombre}}</h4>
                                 </div>
-                            </div>
-                            <div class="col-12 wow fadeIn" data-wow-delay="0.5s">
-                                <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
-                                    <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 90px; height: 90px;">
-                                        <img src="{{$proximoPartido->equipoVisitante->imagen}}" alt="{{$proximoPartido->equipoVisitante->nombre}}" width="100">
+                                <div class="col-12 wow fadeIn" data-wow-delay="0.5s">
+                                    <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
+                                        <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 90px; height: 90px;">
+                                            <img src="{{$proximoPartido->equipoVisitante->imagen}}" alt="{{$proximoPartido->equipoVisitante->nombre}}" width="100">
+                                        </div>
+                                        <h4 class="mb-0">{{$proximoPartido->equipoVisitante->nombre}}</h4>
                                     </div>
-                                    <h4 class="mb-0">{{$proximoPartido->equipoVisitante->nombre}}</h4>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 wow fadeIn" data-wow-delay="0.7s">
-                        <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
-                            <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 90px; height: 90px;">
-                                <i class="fa fa-calendar fa-3x text-primary"></i>
+                        <div class="col-md-6 wow fadeIn" data-wow-delay="0.7s">
+                            <div class="text-center rounded py-5 px-4" style="box-shadow: 0 0 45px rgba(0,0,0,.08);">
+                                <div class="btn-square bg-light rounded-circle mx-auto mb-4" style="width: 90px; height: 90px;">
+                                    <i class="fa fa-calendar fa-3x text-primary"></i>
+                                </div>
+                                <h4 class="mb-0">{{date('d.m.Y', strtotime($proximoPartido->fecha))}}</h4>
+                                <h3 class="mb-0">{{date('H:i', strtotime($proximoPartido->fecha))}}</h3>
+                                <p>{{$proximoPartido->pabellon}} </br> {{$proximoPartido->localidad}}</p>
                             </div>
-                            <h4 class="mb-0">{{date('d.m.Y', strtotime($proximoPartido->fecha))}}</h4>
-                            <h3 class="mb-0">{{date('H:i', strtotime($proximoPartido->fecha))}}</h3>
-                            <p>{{$proximoPartido->pabellon}} </br> {{$proximoPartido->localidad}}</p>
                         </div>
                     </div>
-                </div>
+                @endif
             </div>
         </div>
     </div>
@@ -221,6 +227,26 @@
                         </div>
                         <div class="d-flex">
                             <p class="text-white">{{$jugadora->posicione->nombre}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            @php
+                $decimaSegundo++;
+            @endphp
+            @endforeach
+            
+            @foreach ($staffPrimerEquipo as $staff)
+            <div class="col-lg-4 col-md-6 portfolio-item second wow fadeInUp" data-wow-delay="0.{{$decimaSegundo}}s">
+                <div class="portfolio-inner rounded">
+                    <img class="img-fluid" src="{{$staff->imagen}}" alt="">
+                    <div class="portfolio-text">
+                        <h4 class="text-white mb-4">{{$staff->nombre}}</h4>
+                        <div class="d-flex">
+                            <a class="btn btn-lg-square rounded-circle mx-2 bold-text"><i class="fa fa-star "></i></a>
+                        </div>
+                        <div class="d-flex">
+                            <p class="text-white">{{$staff->role->nombre}}</p>
                         </div>
                     </div>
                 </div>
