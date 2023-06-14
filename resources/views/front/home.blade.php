af9720745c20e0fee3fd4c005ced832c5596a1c7
davidluengos <davidluengosanchez@hotmail.com>
2023-06-14 11:06:14 +0200

    añadidas instrucciones readme

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -190,7 +190,7 @@
                 <span class="fs-5 fw-semi-bold text-light">CB Al-Qázeres</span>
             </div>
             <div class="col-sm-6 col-lg-3 text-center wow fadeIn" data-wow-delay="0.7s">
-                <h1 class="display-4 text-white" data-toggle="counter-up">1234</h1>
+                <h1 class="display-4 text-white" data-toggle="counter-up">1333</h1>
                 <span class="fs-5 fw-semi-bold text-light">CB Al-Qázeres</span>
             </div>
         </div>
